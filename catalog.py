82ee696f06e39660147032508ada4e7703221c63
Joel Young <joel@jyng.com>
2017-11-27 15:28:37 -0500

    Replaced manage.py with catalog.py

--- catalog.py
+++ catalog.py
@@ -1,12 +1,15 @@
 import os
-from flask_script import Server, Manager
-from flask import g
+# from flask_script import Server, Manager
+# from flask import g
 from app import create_app
+from flask import g
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
-manager.add_command("runserver", Server(host="0.0.0.0", port=5000))
+foo="Hi there"
 
+# @app.route('/')
+# def index():
+#     return '<h1>Hello</h1'
 
 @app.before_request
 def before_request():
@@ -20,11 +23,6 @@ def close_session(error):
     if hasattr(g, 'db_session'):
         g.db_session.close()
 
-
-@manager.command
-def hello():
-    print("Hello")
-
-
-if __name__ == "__main__":
-    manager.run()
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
