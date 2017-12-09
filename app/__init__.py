b731b19c90d2794e09c4c14603e6e8b3e898a6ed
Joel Young <joel@jyng.com>
2017-12-09 12:07:00 -0500

    Uncommented has_no_empty_params()

--- app/__init__.py
+++ app/__init__.py
@@ -44,9 +44,9 @@ def create_app(config_name):
 
     return app
 
-# def has_no_empty_params(rule):
-#     defaults = rule.defaults if rule.defaults is not None else()
-#     arguments = rule.arguments if rule.arguments is not None else()
-#     return len(defaults) >= len(arguments)
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else()
+    arguments = rule.arguments if rule.arguments is not None else()
+    return len(defaults) >= len(arguments)
 
 
