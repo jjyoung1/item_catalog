8fc7eb07165bd5744ac42dcf2fc671e3c9126139
Joel Young <joel@jyng.com>
2017-12-15 11:04:16 -0500

    Fixed warnings on not closing files for security info

--- secrets/__init__.py
+++ secrets/__init__.py
@@ -2,38 +2,43 @@ import os
 import json
 
 ''' Google Client Secrets'''
-class GoogleClientSecrets:
 
+
+class GoogleClientSecrets:
     gcs = None
     path = None
+
     def __init__(self, filename):
         self.path = os.path.join(basedir, filename)
-        self.gcs = json.loads(
-            open(self.path, 'r').read())
+        with open(self.path, 'r') as fp:
+            self.gcs = json.loads(fp.read())
 
     def get_client_id(self):
         return self.gcs['web']['client_id']
 
     def get_secrets_path(self):
         return self.path
+
 
 basedir = os.path.abspath(os.path.dirname(__file__)) + '/'
 
 google_client_secrets = GoogleClientSecrets('client_secrets.json')
 
-class FacebookClientSecrets:
 
+class FacebookClientSecrets:
     fcs = None
     path = None
+
     def __init__(self, filename):
         self.path = os.path.join(basedir, filename)
-        self.fcs = json.loads(
-            open(self.path, 'r').read())
+        with open(self.path, 'r') as fp:
+            self.fcs = json.loads(fp.read())
 
     def get_client_id(self):
         return self.gcs['web']['client_id']
 
     def get_secrets_path(self):
         return self.path
+
 
 facebook_client_secrets = FacebookClientSecrets('fb_client_secrets.json')
